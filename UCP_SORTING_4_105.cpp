7191f2e4e1bc486319d0bfc2f8e6e8988e1028a8
xerro <nashrul.fikri19@gmail.com>
2024-03-22 10:46:41 +0700

    Add project files.

---
+++ UCP_SORTING_4_105.cpp
@@ -0,0 +1,19 @@
+// UCP_SORTING_4_105.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+//1.cara insertion sort menukar dengan elemen lain adalah dengan menukar kemungkinan bilangan angka paling besar menuju kebagian paling kanan hingga yang paling kecil di bagian kiri
+
+//2.cara algoritma slection sort menukar dengan elemen lain adalah  dengan menukar kemungkinan bilangan angka paling  besar menuju ke paling kiri hingga bagian yang paling kecil di bagian kanan
+
+//3.untuk yang pertama kita melihat jumlah angka yang berada didalam kotak, yang kedua mulai menukarkan dari yang paling besar menuju ke kiri, seperti contoh 11,13,23,21,12 dari contoh tersebut bisa diketahui bahwa angka yang paling besar adalah 23 lalu setelah mengetahui mana angka yang paling besar hanya tinggal di swap kekanan ditukar dengan 23-21,23-12 dan seterusnya
+  
+//4.s = n + sum( n -1 )
+
+// n = 5
+// i = 3
+// temp = 21
+// j = 11
+
+// n = 5
+// i = 2
+// temp = 23
+// j = -1
